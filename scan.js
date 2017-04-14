663bc17acbb65ddfea11801eacc9dfead1cd73ee
Christoph <ckluente@thoughtworks.com>
2017-04-14 21:08:14 +0200

    send auth header when POSTing

--- scan.js
+++ scan.js
@@ -1,5 +1,6 @@
 const config = require('config');
 const request = require('request');
+const moment = require('moment');
 const LinuxInputListener = require('linux-input-device');
 
 var apiurl = config.get('pace-url')+'/api/scan';
@@ -18,10 +19,8 @@ events.forEach(function(event) {
 
 var number = {};
 
+
 function handle_keys(key,event) {
-  var paceRequest = request.defaults({
-      headers: {'X-Pace-Token': config.get(token)}
-  })
   if (keylist.indexOf(key) >= 0)  {
     if (typeof number[event] === "undefined") {
       number[event] = '';
@@ -30,20 +29,41 @@ function handle_keys(key,event) {
     number[event] = "" + number[event] + (key-1)
   }
   if (key == 28) {
-    paceRequest.post(apiurl, {form:{startnumber:input,time:Date.now()}})
-      .on('response', function(response) {
-       console.log(response);
-      })
-    .on('error', function(err) {
-      console.log('error from server');
-    });
+    sendResult(input,Date.now());
     number[event] = '';
-  }
+  };
 };
 
 function exitHandler(options, err) {
     process.exit();
 }
+
+
+function sendResult(startnumber,time) {
+  var options = {
+      url: apiurl,
+      method: 'POST',
+      form: {startnumber:startnumber,time:time},
+      headers: {
+            'X-Pace-Token': config.get('token')
+          }
+  };
+  request(options, handleResponse);
+};
+
+function finishSeconds() {
+  return moment().unix() - moment().startOf('day').unix();
+};
+
+function handleResponse(error, response, body) {
+    if (!error ) {
+      console.log(response.statusCode);
+    }
+   else {
+     console.log(error)
+   }
+}
+ 
 
 process.on('exit', exitHandler.bind(null, {exit:true}));
 
